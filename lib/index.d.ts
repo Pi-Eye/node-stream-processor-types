74bdc978cf1d22154f26520a4f40c619beca229b
bwu1324 <57691028+bwu1324@users.noreply.github.com>
2022-10-01 00:31:52 -0500

    feat: export processed frame type

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 import * as SPTypes from "./stream_processor_types";
 import * as SPEnums from "./stream_processor_enums";
-export { SPTypes, SPEnums };
+import { Processed } from "./stream_processor_types";
+export { SPTypes, SPEnums, Processed };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as SPTypes from "./stream_processor_types";
 import * as SPEnums from "./stream_processor_enums";
+import { Processed } from "./stream_processor_types";
 
-export { SPTypes, SPEnums };
+export { SPTypes, SPEnums, Processed };
